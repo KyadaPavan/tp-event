49f908ff9e0cb6dd6ff44370a62a0aa5943bcbb2
Kyada Pavan <kyadapavan0016@gmail.com>
2025-02-21 14:20:22 +0530

    update

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -5,7 +5,12 @@ const CTA = () => {
   return (
     <>
       <div className="md:max-w-[90%] w-full mx-auto ">
-        <div className="md:w-fit md:h-fit w-[80] h-[80] overflow-x-hidden">
+        <div className="hidden md:block">
+          <Boxes />
+          <FAQS />
+        </div>
+
+        <div className="md:w-fit md:h-fit w-[80] h-[80] overflow-x-hidden md:hidden block">
           <Boxes />
           <FAQS />
         </div>
